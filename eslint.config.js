ebbc67f40daced535b629e0640604522fa066864
theparitet <200591210+theParitet@users.noreply.github.com>
2025-12-17 02:38:36 +0400

    fix: adjust the path for github pages deployment and modify eslint config to account for node.js environment

--- eslint.config.js
+++ eslint.config.js
@@ -26,4 +26,10 @@ export default defineConfig([
             'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
         },
     },
+    {
+        files: ['vite.config.js', 'eslint.config.js'],
+        languageOptions: {
+            globals: globals.node,
+        },
+    }
 ]);
